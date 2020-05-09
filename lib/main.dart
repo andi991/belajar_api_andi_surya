e170562379087807e9b9ac392e578d66043a09c6
andi991 <andisurya991@gmail.com>
2020-05-09 20:36:30 +0800

    belajar api dengan flutter

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import './home.dart';
+import './tambah_data.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+     theme: ThemeData(
+        primaryColor: Colors.black,
+        accentColor: Colors.white,
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+                color: Colors.black,
+              ),
+              body2: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w200,
+                  color: Colors.black,
+                  height: 1.5),
+            ),
+      ),
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "BELAJAR API",
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              icon: Icon(Icons.person_add),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => TambahScreen(),
+                    ));
+              }),
+        ],
+      ),
+      body: HomeScreen(),
+    );
+  }
+}
